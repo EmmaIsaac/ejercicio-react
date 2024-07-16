a43489603184672d1baba972dd463b16daf843a3
EmmaIsaac <emmanuel.i157@yahoo.com.ar>
2024-07-16 19:43:05 -0300

    000

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,25 @@
 import Section from "../components/Section";
 
 const Main = () => {
-  return (
-    <main>
-      <Section
-        ident="inicio"
-        titulo="Inicio"
-        parrafo="Lorem 1234 ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit atque doloribus alias numquam enim rem ipsa ex nulla, molestias iure magni expedita magnam libero provident sapiente est nesciunt laborum deserunt?"
-      />
-      <Section
-        ident="caracteristicas"
-        titulo="Caracteristicas"
-        parrafo="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit atque doloribus alias numquam enim rem ipsa ex nulla, molestias iure magni expedita magnam libero provident sapiente est nesciunt laborum deserunt?"
-      />
-      <Section
-        ident="contacto"
-        titulo="Contacto"
-        parrafo="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit atque doloribus alias numquam enim rem ipsa ex nulla, molestias iure magni expedita magnam libero provident sapiente est nesciunt laborum deserunt?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit atque doloribus alias numquam enim rem ipsa ex nulla, molestias iure magni expedita magnam libero provident sapiente est nesciunt laborum deserunt?"
-      />
-    </main>
-  );
+    return (
+        <main>
+            <Section
+                ident="inicio"
+                titulo="Inicio"
+                parrafo="Lorem 12340000 ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit atque doloribus alias numquam enim rem ipsa ex nulla, molestias iure magni expedita magnam libero provident sapiente est nesciunt laborum deserunt?"
+            />
+            <Section
+                ident="caracteristicas"
+                titulo="Caracteristicas"
+                parrafo="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit atque doloribus alias numquam enim rem ipsa ex nulla, molestias iure magni expedita magnam libero provident sapiente est nesciunt laborum deserunt?"
+            />
+            <Section
+                ident="contacto"
+                titulo="Contacto"
+                parrafo="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit atque doloribus alias numquam enim rem ipsa ex nulla, molestias iure magni expedita magnam libero provident sapiente est nesciunt laborum deserunt?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit atque doloribus alias numquam enim rem ipsa ex nulla, molestias iure magni expedita magnam libero provident sapiente est nesciunt laborum deserunt?"
+            />
+        </main>
+    );
 };
 
 export default Main;
